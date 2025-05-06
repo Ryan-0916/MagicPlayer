a0a153d01b3a62ce9a2c2f18ab7c534bb9aed71c
Ryan0916 <ningrui0916@gmail.com>
2025-05-06 15:14:09 +0800

    2025-05-06
    1. 优化了一部分代码

--- core/src/main/java/com/magicrealms/magicplayer/core/repository/PlayerDataRepository.java
+++ core/src/main/java/com/magicrealms/magicplayer/core/repository/PlayerDataRepository.java
@@ -6,7 +6,6 @@ import com.magicrealms.magiclib.common.store.RedisStore;
 import com.magicrealms.magiclib.common.utils.MongoDBUtil;
 import com.magicrealms.magicplayer.core.player.PlayerData;
 import com.mongodb.client.MongoCursor;
-import com.mongodb.client.model.Filters;
 import org.bson.Document;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.Nullable;
@@ -17,9 +16,6 @@ import java.util.function.Consumer;
 /**
  * @author Ryan-0916
  * @Desc TODO：一段话介绍该类对象
- * 例如我现在要创建一个 MongoDB 的对象，比如玩家对象，第一步创建实体类
- *
- *
  * @date 2025-05-06
  */
 public class PlayerDataRepository extends BaseRepository<PlayerData> {
@@ -28,29 +24,26 @@ public class PlayerDataRepository extends BaseRepository<PlayerData> {
                                 String tableName,
                                 @Nullable RedisStore redisStore,
                                 long cacheExpire) {
-        super(mongoDBStore, tableName, redisStore, cacheExpire, PlayerData.class);
+        super(mongoDBStore, tableName, redisStore, true, cacheExpire, PlayerData.class);
     }
 
     public PlayerData queryByPlayer(Player player) {
-        String subKey = player.getName();
-        Optional<PlayerData> redisData = Optional.ofNullable(super.getRedisStore())
-                .flatMap((r) -> r.hGetObject(super.getCacheHkey(), subKey, PlayerData.class));
-        if (redisData.isPresent()) {
-            return redisData.get();
+        String id = player.getName();
+        Optional<PlayerData> cachedData = super.getRedisStore().
+                hGetObject(super.getCacheHkey(), id, PlayerData.class);
+        if (cachedData.isPresent()) {
+            return cachedData.get();
         }
-        try (MongoCursor<Document> iterator = super.getMongoDBStore()
-                .select(super.getTableName(), Filters.eq("name", subKey))) {
+        try (MongoCursor<Document> cursor = super.getMongoDBStore()
+                .select(super.getTableName(), super.getIdFilter(id))) {
             PlayerData data;
-            if (iterator.hasNext()) {
-                data = MongoDBUtil.toObject(iterator.next(), PlayerData.class);
-                Optional.ofNullable(super.getRedisStore())
-                        .ifPresent(r -> r.hSetObject(super.getCacheHkey(), subKey, data, super.getCacheExpire()));
+            if (cursor.hasNext()) {
+                data = MongoDBUtil.toObject(cursor.next(), PlayerData.class);
             } else {
                 data = new PlayerData(player);
-                insert(data);
-                Optional.ofNullable(super.getRedisStore())
-                        .ifPresent(r -> r.hSetObject(super.getCacheHkey(), subKey, data, super.getCacheExpire()));
+                super.insert(data);
             }
+            super.cacheEntity(id, data);
             return data;
         } finally {
             super.getMongoDBStore().close();
